83c7b43ac57ff6736ca1f55b929bf83d7afbd2dc
WambuiPeris <periswambui.k@gmail.com>
2021-08-31 12:14:06 -0700

    My first commit

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * main - check the code for ALX School students.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-void print_alphabet(void);
 int main(void)
 {
     print_alphabet();
